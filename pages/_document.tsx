5c88525de99a343c69a60fe21c65e9fb68aa97bf
Moustafa Elhadary <MoustafaElhadary96@gmail.com>
2023-04-08 21:43:56 -0400

    Update doc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,32 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+      
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#9196A1" />
+
           <script
             async
             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVYSep9hE8ehsPlGltm_7qgcv_EoqfyCY&libraries=places"
